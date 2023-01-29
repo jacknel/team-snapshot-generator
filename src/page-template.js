b9598ccbfcaedb1d6ac2df2d4dc1da22c1d15508
jacknel <mjacknelson19@gmail.com>
2023-01-28 21:24:06 -0600

    finished functionality, last thing to finish is tests

--- src/page-template.js
+++ src/page-template.js
@@ -1,12 +1,54 @@
 const assembleTeam = team => {
     const loadManager = manager => {
-        return ``;
+        return `
+        <div class="card em-card">
+            <div class="card-header">
+                <h2 class="card-title">${manager.getName()}</h2>
+                <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
+            </div>
+            <div class="card-content">
+                <ul class="list-group">
+                    <li class="list-group-obj">ID: ${manager.getId()}</li>
+                    <li class="list-group-obj">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
+                    <li class="list-group-obj">Office number: ${manager.getOfficeNumber()}</li>
+                </ul>
+            </div>
+        </div>
+        `;
     };
     const loadEngineer = engineer => {
-        return ``;
+        return `
+        <div class="card em-card">
+            <div class="card-header">
+                <h2 class="card-title">${engineer.getName()}</h2>
+                <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${engineer.getRole()}</h3>
+            </div>
+            <div class="card-content">
+                <ul class="list-group">
+                    <li class="list-group-obj">ID: ${engineer.getId()}</li>
+                    <li class="list-group-obj">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
+                    <li class="list-group-obj">GitHub: <a href="https://github.com/${engineer.getGithub()}" target="_blank" rel="noopener noreferrer">${engineer.getGithub()}</a></li>
+                </ul>
+            </div>
+        </div>
+        `;
     };
     const loadIntern = intern => {
-        return ``;
+        return `
+        <div class="card em-card">
+            <div class="card-header">
+                <h2 class="card-title">${intern.getName()}</h2>
+                <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${intern.getRole()}</h3>
+            </div>
+            <div class="card-content">
+                <ul class="list-group">
+                    <li class="list-group-obj">ID: ${intern.getId()}</li>
+                    <li class="list-group-obj">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
+                    <li class="list-group-obj">School: ${intern.getSchool()}</li>
+                </ul>
+            </div>
+        </div>
+        `;
     };
 
     const html = [];
@@ -30,5 +72,37 @@ const assembleTeam = team => {
 }
 
 module.exports = (team) => {
-    return ``;
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <title>Team</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+            integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+        <script src="https://kit.fontawesome.com/c502137733.js"></script>
+    </head>
+
+    <body>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12 jumbotron mb-3 team-head">
+                    <h1 class="text-center">Team</h1>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="team-section col-12 d-flex justify-content-center">
+                    ${assembleTeam(team)}
+                </div>
+            </div>
+        </div>
+    </body>
+    </html>
+    `;
 };
