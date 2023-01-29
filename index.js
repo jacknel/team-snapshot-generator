b4da9c6353c959c9bba9048cf8e6440cd44a0b9f
jacknel <mjacknelson19@gmail.com>
2023-01-28 20:48:11 -0600

    finished index functionality

--- index.js
+++ index.js
@@ -59,22 +59,140 @@ function runApp() {
     }
 
     function createTeam() {
-
+        inquirer
+            .prompt([
+                {
+                    type: 'list',
+                    name: 'memberChoice',
+                    message: 'What would you like to do?',
+                    choices: [
+                        'Add Engineer',
+                        'Add Intern',
+                        'Finish Building Team',
+                    ],
+                }
+            ])
+            .then((userChoice) => {
+                switch (userChoice.memberChoice) {
+                    case 'Add Engineer':
+                        inputEngineer();
+                        break;
+                    case 'Add Intern':
+                        inputIntern();
+                        break;
+                    default:
+                        buildTeam();        
+                }
+            });
     }
 
     function inputEngineer() {
-
+        inquirer
+            .prompt([
+                {
+                    type: 'input',
+                    name: 'engineerName',
+                    message: 'Enter engineer name.',
+                    validate: (answer) => (answer !== "") || "Please enter at least one character."
+                },
+                {
+                    type: 'input',
+                    name: 'engineerId',
+                    message: 'Enter engineer ID.',
+                    validate: (answer) => {
+                        const pass = answer.match(/^[1-9]\d*$/);
+                        if (pass) {
+                          if (idArray.includes(answer)) {
+                            return 'This ID is already taken. Enter a different number.';
+                          } else {
+                            return true;
+                          }
+                        }
+                        return 'Enter a positive number greater than zero.';
+                      },
+                },
+                {
+                    type: 'input',
+                    name: 'engineerEmail',
+                    message: 'Enter engineer email.',
+                    validate: (answer) => (answer.match(/\S+@\S+\.\S+/) && true) || "Enter a valid email address."
+                },
+                {
+                    type: 'input',
+                    name: 'engineerGithub',
+                    message: 'Enter engineer Github username.',
+                    validate: (answer) => (answer !== "") || "Please enter at least one character.",
+                },
+            ])
+            .then((answers) => {
+                const engineer = new Engineer(
+                    answers.engineerName,
+                    answers.engineerId,
+                    answers.engineerEmail,
+                    answers.engineerGithub
+                );
+                teamArray.push(engineer);
+                idArray.push(answers.engineerId);
+                createTeam();
+            });
     }
 
     function inputIntern() {
-
+        inquirer
+            .prompt([
+                {
+                    type: 'input',
+                    name: 'internName',
+                    message: 'Enter intern name.',
+                    validate: (answer) => (answer !== "") || "Please enter at least one character."
+                },
+                {
+                    type: 'input',
+                    name: 'internId',
+                    message: 'Enter intern ID.',
+                    validate: (answer) => {
+                        const pass = answer.match(/^[1-9]\d*$/);
+                        if (pass) {
+                          if (idArray.includes(answer)) {
+                            return 'This ID is already taken. Enter a different number.';
+                          } else {
+                            return true;
+                          }
+                        }
+                        return 'Enter a positive number greater than zero.';
+                      },
+                },
+                {
+                    type: 'input',
+                    name: 'internEmail',
+                    message: 'Enter intern email.',
+                    validate: (answer) => (answer.match(/\S+@\S+\.\S+/) && true) || "Enter a valid email address."
+                },
+                {
+                    type: 'input',
+                    name: 'internSchool',
+                    message: 'Enter intern school.',
+                    validate: (answer) => (answer !== "") || "Please enter at least one character.",
+                },
+            ])
+            .then((answers) => {
+                const intern = new Intern(
+                    answers.internName,
+                    answers.internId,
+                    answers.internEmail,
+                    answers.internSchool
+                );
+                teamArray.push(intern);
+                idArray.push(answers.internId);
+                createTeam();
+            });
     }
 
     function buildTeam() {
+        fs.writeFileSync(distPath, render(teamArray), "utf-8");
+      }
 
-    }
-
-    createManager();
+    inputManager();
 }
 
 runApp();
